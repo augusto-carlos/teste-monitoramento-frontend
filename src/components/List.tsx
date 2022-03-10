ef5f01407c65b8f9aa7606d3de82fba6b80b5ee3
Carlos Augusto <62240490+augusto-carlos@users.noreply.github.com>
2022-03-10 07:49:19 +0100

    feat: change the cols depending on the screen

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const List = ({ items }: Props) => (
-  <ul className="grid grid-cols-4 gap-4">
+  <ul className="grid grid-cols-2 md:grid-cols-4 gap-4">
     {items.map(item => (
       <li key={item.id}>
         <ListItem user={item} />
